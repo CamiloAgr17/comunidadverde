ebda7f32a75f3cf72bbfadad9d928379f5cd6cd8
CamiloAgr17 <cagrazalesc@gmail.com>
2025-07-17 03:51:08 -0500

    Arreglos de View para Mapa
    Permite localizar en el mapa a las organizaciones que crearon su cuenta.

--- mapa/views.py
+++ mapa/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from .models import CentroReciclaje
 from django.utils.safestring import mark_safe
 import json
+from inicio.models import Organizacion
 
 def mapa_view(request):
-    centros = CentroReciclaje.objects.all().values('nombre', 'lat', 'lng', 'materiales')
-    centros_json = mark_safe(json.dumps(list(centros)))
-    return render(request, "mapa/index.html", {"centros_json": centros_json})
+    organizaciones_qs = Organizacion.objects.exclude(latitud__isnull=True, longitud__isnull=True)\
+        .values('nombre', 'latitud', 'longitud')
+    
+    organizaciones = []
+    for org in organizaciones_qs:
+         organizaciones.append({
+            'nombre': org['nombre'],
+            'latitud': float(org['latitud']),
+            'longitud': float(org['longitud']),
+        })
+
+    organizaciones_json = mark_safe(json.dumps(list(organizaciones)))
+    return render(request, "mapa/index.html", {"organizaciones_json": organizaciones_json})
+
+
